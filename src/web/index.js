60a85523bc6f792d52cff1413397932821ac0f51
jordan Qiu <u6421729@anu.edu.au>
2024-09-16 17:01:24 +1000

    improvements on website. fixed layout issues.

--- src/web/index.js
+++ src/web/index.js
@@ -7,18 +7,20 @@ $(document).ready(function () {
 
     var video = $("#video");
 
+    var vidwrapper = $("#vidwrapper");
+
     var camarr = [1, 2, 3, 4];
 
     var counter = 0;
 
     var numberofcams = camarr.length;
 
-    var buttonnext = "<button id='next'>Next &raquo;</button>";
+    var buttonnext = "<button class='cycle' id='next'>Next &raquo;</button>";
 
-    var buttonprevious = "<button id='previous'>&laquo; Previous</button>";
+    var buttonprevious = "<button class='cycle' id='previous'>&laquo; Previous</button>";
 
-    body.append(
-        buttonprevious + buttonnext
+    vidwrapper.append(
+        "<div id='buttons'><div id='prevcontainer' class='buttoncontainer'>" + buttonprevious + "</div><div id='nextcontainer' class='buttoncontainer'>" + buttonnext + "</div></div>"
     );
     $("#next").on("click", function () {
         counter = (counter + 1) % numberofcams;
