60073e0291a8396b10465288409549c39350bc24
UC-Talha Ahmed <u3240708@uni.canberra.edu.au>
2025-03-05 17:17:58 +1100

    Smart Gate Web App

---
+++ src/main-web/app.py
@@ -0,0 +1,10 @@
+from flask import Flask
+from controllers.main_controller import test_bp, data_bp
+
+app = Flask("smartgate")
+
+app.register_blueprint(test_bp)
+app.register_blueprint(data_bp)
+
+if __name__ == "__main__":
+    app.run(debug=True)
