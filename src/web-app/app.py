83555eb7653d31f602ac7742fc6549a97808c25b
talned <u3240708@uni.canberra.edu.au>
2025-03-29 15:48:56 +1100

    fixed something

--- src/web-app/app.py
+++ src/web-app/app.py
@@ -15,4 +15,4 @@ app.include_router(root_router)
 if __name__ == "__main__":
     # Running the uvicorn as a webserver
     import uvicorn
-    uvicorn.run("app:app", host="127.0.0.1", port=8000, reload=True)
+    uvicorn.run("app:app", host="0.0.0.0", port=8000, reload=True)
