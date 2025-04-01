88c155d3244ca41eefbf58d8736674f6583999a9
talned <u3240708@uni.canberra.edu.au>
2025-04-01 15:02:25 +1100

    Updated frontend

--- src/web-app/app.py
+++ src/web-app/app.py
@@ -16,4 +16,4 @@ app.include_router(root_router)
 if __name__ == "__main__":
     # Running the uvicorn as a webserver
     import uvicorn
-    uvicorn.run("app:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("app:app", host="0.0.0.0", port=8000, reload=True, reload_dirs=["wwwroot"])
