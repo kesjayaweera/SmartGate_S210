69a9613ed5b81c92f81de96900ce4701de0ea281
talned <u3240708@uni.canberra.edu.au>
2025-04-03 23:57:22 +1100

    Added cleanup support for windows

--- src/web-app/cleanup.ps1
+++ src/web-app/cleanup.ps1
@@ -1,6 +1,6 @@
-#!/bin/bash
+# cleanup.ps1
 
-# Remove volume and stop related containers
+# Stop and remove containers and volume
 docker stop buildx_buildkit_dockerbuilder0
 docker rm buildx_buildkit_dockerbuilder0 web-app-sgwebimage-1 web-app-postgres-1
 docker volume rm web-app_smartgate-data
