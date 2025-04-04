68ff6057ac4567e744bb4985e03e4b853e193e47
talned <u3240708@uni.canberra.edu.au>
2025-04-04 23:36:41 +1100

    Made changes to the animal_schema.sql

--- src/web-app/data/animal_schema.sql
+++ src/web-app/data/animal_schema.sql
@@ -1,16 +1,14 @@
-CREATE TABLE IF NOT EXISTS species_list (
-    species_id SERIAL PRIMARY KEY,
-    species_name VARCHAR(255) UNIQUE NOT NULL,
+CREATE TABLE IF NOT EXISTS animals (
+    time_stamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    animal_id SERIAL PRIMARY KEY,
+    animal_type VARCHAR(255) NOT NULL,
+    species_name VARCHAR(255) NOT NULL,
+    has_detected BOOLEAN DEFAULT FALSE,
+    has_entered BOOLEAN DEFAULT FALSE,
     is_endangered BOOLEAN NOT NULL,
     is_threat BOOLEAN NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS animals (
-    animal_id SERIAL PRIMARY KEY,
-    animal_type VARCHAR(255) NOT NULL,
-    species_id INT REFERENCES species_list(species_id) ON DELETE SET NULL,
-    has_detected BOOLEAN DEFAULT FALSE,
-    has_entered BOOLEAN DEFAULT FALSE,
-    is_friendly BOOLEAN NOT NULL
-);
+
+-- Add sql functions to get statistics about the animals
 
