ce347385beb0332f6d8d1ce5b3bc4a1f4199c3c1
talned <u3240708@uni.canberra.edu.au>
2025-04-22 14:46:13 +1000

    Added comments and new tables to gate model

--- src/web-app/models/gate_schema.sql
+++ src/web-app/models/gate_schema.sql
@@ -1,5 +1,6 @@
+-- Add a table for animals potentially detected by gate
 CREATE TABLE IF NOT EXISTS detected_animals (
-    time_stamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    time_stamp TIMESTAMP DEFAULT NOW(),
     animal_id SERIAL PRIMARY KEY,
     animal_type VARCHAR(255) NOT NULL,
     animal_name VARCHAR(255) NOT NULL,
@@ -7,6 +8,7 @@ CREATE TABLE IF NOT EXISTS detected_animals (
     is_threat BOOLEAN NOT NULL
 );
 
+-- Provide some mock data to show what animals are potentially detected
 INSERT INTO detected_animals (animal_type, animal_name, is_endangered, is_threat)
 VALUES
     ('Mammal', 'Amur Leopard', TRUE, TRUE),
@@ -85,6 +87,24 @@ VALUES
     ('Mammal', 'Fox', FALSE, FALSE),
     ('Mammal', 'Snow Leopard', TRUE, TRUE);
 
+-- Creating some predictable data for alerts
+CREATE TYPE alert_level_enum AS ENUM ('info', 'warning', 'critical')
 
--- Add sql functions to get statistics about the animals
+-- Add table for alerts 
+CREATE TABLE IF NOT EXISTS alerts (
+    alert_no SERIAL PRIMARY KEY,
+    alert_desc TEXT,
+    alert_level alert_level_enum,
+    date_and_time TIMESTAMP DEFAULT NOW()
+);
 
+-- Creating some predictable data for gates
+CREATE TYPE gate_status_enum AS ENUM ('opened', 'closed')
+CREATE TYPE gate_condition_enum AS ENUM ('unavailable', 'available')
+
+-- Add table for gates
+CREATE TABLE IF NOT EXISTS gates (
+    gate_no SERIAL PRIMARY KEY,
+    gate_status gate_status_enum, -- opened or closed
+    gate_condition gate_condition_enum
+)
