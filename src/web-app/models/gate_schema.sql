409671b2db8fe88c47596bf6920c18b296eb1d50
talned <u3240708@uni.canberra.edu.au>
2025-04-25 02:54:03 +1000

    Moved time_stamp to the bottom in detected_animals

--- src/web-app/models/gate_schema.sql
+++ src/web-app/models/gate_schema.sql
@@ -1,11 +1,11 @@
 -- Add a table for animals potentially detected by gate
 CREATE TABLE IF NOT EXISTS detected_animals (
-    time_stamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     animal_id SERIAL PRIMARY KEY,
     animal_type VARCHAR(255) NOT NULL,
     animal_name VARCHAR(255) NOT NULL,
     is_endangered BOOLEAN NOT NULL,
-    is_threat BOOLEAN NOT NULL
+    is_threat BOOLEAN NOT NULL,
+    time_stamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
 -- Creating some predictable data for alerts
